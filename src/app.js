f8dcd9d70e74fa09ac01a24416666ea8cc38cec2
douglasdev <doug012310@yahoo.com>
2019-11-22 17:02:39 -0500

    fixed legend, tried to add labels to each state but it's not working

--- src/app.js
+++ src/app.js
@@ -18,12 +18,14 @@ let colorScale = d3.scaleQuantile()
   .domain(domain)
   .range(d3.schemeBlues[7]);
 
-console.log("schemeBlues: ", d3.schemeBlues[7])
-console.log("colorScale: ", colorScale)
-console.log(colorScale.invertExtent(d3.schemeBlues[7][0]))
+// console.log("schemeBlues: ", d3.schemeBlues[7])
+// console.log("colorScale: ", colorScale)
+// console.log(colorScale.invertExtent(d3.schemeBlues[7][0]))
 
 let path = d3.geoPath()
   .projection(d3.geoAlbersUsa())
+
+console.log(path)
 
 let svg = document
   .createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -37,55 +39,79 @@ d3.select("svg")
   .enter()
   .append("path")
   .attr("d", path)
-  .style("fill", function(d) {
-    return colorScale(d.energy["Production, U.S. Share"])
-})
+  .style("fill", d => colorScale(d.energy["Production, U.S. Share"]))
+  .attr("class", "state")
+  
+
+
+d3.selectAll(".state")
+  .append("g")
+  // .attr("transform", "translate(102 0)")
+  .append("text")
+  .attr("x", d=>path.centroid(d)[0])
+  .attr("y",d=> path.centroid(d)[1])
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "10px")
+  .attr("fill", "black")
+  .text(d=>d.properties.NAME)
+  .on("mouseover", function(d){
+
+  })
+  .on("mouseout", function(d){
+
+  });
+
 
 // Legend
 d3.select("svg")
   .append("g")
   .attr("class", "legend")
+  .attr("id", "main")
 
 //Legend background 
-d3.select("g.legend")
+d3.select("g#main")
   .append('rect')
   .style("fill", () => "#ffffff")
   .attr("width", 100)
   .attr("height", 135)
   .attr("transform", "translate(100 0)")
+ .style("stroke", "black")
+ .style("stroke-width", 1)
 
 // Legend boxes
 let legend = d3
-  .select("g")
+  .select("g#main")
   .selectAll('g.legendEntry')
   .data(d3.schemeBlues[7])
   .enter()
   .append('g')
   .attr('class','legendEntry')
-  .attr("transform", "translate(100 0)")
+  .attr("transform", "translate(102 0)")
 
 legend
   .append('rect')
   .attr("x", 0)
   .attr("y", function(d, i) {
-     return i * 20;
+     return i * 20+2;
   })
  .attr("width", 10)
  .attr("height", 10)
  .style("stroke", "black")
  .style("stroke-width", 1)
- .style("fill", function(d){return d;})
+ .style("fill", d=>d)
 
 // Legend text
-let legendText = legend
+let legendTextLabels = 
+  legend
   .append("text")
-
-let legendTextLabels = legendText
   .attr("x", 20)
   .attr("y", function(d, i) {
-    return i * 20;
+    return i * 20+12;
   })
-  .text( function (d) {return d;})
+  .text( function (d) {
+    let range =colorScale.invertExtent(d)
+    return range[0]+' - '+range[1]
+  })
   .attr("font-family", "sans-serif")
   .attr("font-size", "10px")
   .attr("fill", "black");
